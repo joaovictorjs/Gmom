c3edbbd36cbae30bd18b092da1fdc05d1a93fa15
jotave <skai7ami@gmail.com>
2025-02-19 07:42:09 -0300

    create Injector

---
+++ Gmom.Presentation/DependencyInjection/Injector.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+
+namespace Gmom.Presentation.DependencyInjection;
+
+public partial class Injector
+{
+    private static Injector? _injector;
+    public static Injector Instance => _injector ??= new Injector();
+    private readonly ContainerBuilder _builder = new();
+    private readonly IContainer _container;
+
+    private Injector()
+    {
+        MakeRegistrations();
+        _container = _builder.Build();
+    }
+
+    private static void MakeRegistrations()
+    {
+        throw new NotImplementedException();
+    }
+
+    public object Resolve(Type type)
+    {
+        return _container.Resolve(type);
+    }
+
+    public T Resolve<T>()
+        where T : notnull
+    {
+        return _container.Resolve<T>();
+    }
+}
