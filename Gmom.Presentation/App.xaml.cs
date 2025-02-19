33ae44bf7e4a76084409ab9a2d703bbad6934a34
jotave <skai7ami@gmail.com>
2025-02-19 08:01:26 -0300

    handle exceptions

--- Gmom.Presentation/App.xaml.cs
+++ Gmom.Presentation/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Threading;
+using Gmom.Domain.Constants;
+using Gmom.Domain.Interface;
 using Gmom.Presentation.DependencyInjection;
 
 namespace Gmom.Presentation;
@@ -10,12 +13,27 @@ namespace Gmom.Presentation;
 /// </summary>
 public partial class App : Application
 {
+    private readonly Injector _injector = Injector.Instance;
+
     public App()
     {
+        DispatcherUnhandledException += HandleUnhandledExceptions;
     }
 
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
+    }
+
+    private void HandleUnhandledExceptions(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        var message = e.Exception.Message;
+        _injector
+            .Resolve<IMessageBoxService>()
+            .ShowError(
+                $"Ocorreu um erro inesperado! Verifique os arquivo {Filenames.Trace}.\n\n{message}"
+            );
+        message += e.Exception.InnerException?.Message.Insert(0, "\n\n");
+        _injector.Resolve<ILoggerService>().Log(message);
     }
 }
