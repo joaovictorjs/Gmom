20e1b97c0e0b6faf3d135821108172f33b1d8a08
jotave <skai7ami@gmail.com>
2025-02-19 07:43:56 -0300

    project

---
+++ Gmom.Presentation/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Gmom.Presentation.DependencyInjection;
+
+namespace Gmom.Presentation;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+    }
+}
