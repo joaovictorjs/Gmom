89cfe9bbf0f7eac91b06c8ac2d9cc2e7ff0f3a1f
jotave <skai7ami@gmail.com>
2025-02-19 07:43:24 -0300

    implements MessageBoxService

---
+++ Gmom.Presentation/Services/MessageBoxService.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using Gmom.Domain.Interface;
+
+namespace Gmom.Presentation.Services;
+
+public class MessageBoxService : IMessageBoxService
+{
+    public void ShowError(string message)
+    {
+        MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+
+    public void ShowInformation(string message)
+    {
+        MessageBox.Show(message, "Informação", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+
+    public void ShowWarning(string message)
+    {
+        MessageBox.Show(message, "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+    }
+
+    public bool ShowConfirmation(string message)
+    {
+        return MessageBox.Show(
+                message,
+                "Confirmação",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question
+            ) == MessageBoxResult.Yes;
+    }
+}
