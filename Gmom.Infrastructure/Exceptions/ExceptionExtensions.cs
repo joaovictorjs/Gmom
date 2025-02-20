d68b5341736cf1635c8ff472560de791deda6e37
jotave <skai7ami@gmail.com>
2025-02-20 13:59:21 -0300

    create global exception handler

---
+++ Gmom.Infrastructure/Exceptions/ExceptionExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Gmom.Infrastructure.Exceptions;
+
+public static class ExceptionExtensions
+{
+    public static void UseGlobalHandle(this Exception exception, Action<string> action)
+    {
+        GlobalExceptionHandler.Handle(exception, action);
+    }
+}
---
+++ Gmom.Infrastructure/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Gmom.Infrastructure.Exceptions;
+
+public abstract class GlobalExceptionHandler
+{
+    public static void Handle(Exception exception, Action<string> action)
+    {
+        switch (exception)
+        {
+            default:
+                ExceptionDispatchInfo.Capture(exception).Throw();
+                break;
+        }
+    }
+}
