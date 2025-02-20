412a78f0c77b6589973ac1dcd6780d7546b8a282
jotave <skai7ami@gmail.com>
2025-02-20 07:39:47 -0300

    create model contract, and postgres connection store

---
+++ Gmom.Infrastructure/Stores/PostgresConnectionStore.cs
@@ -0,0 +1,17 @@
+﻿using Gmom.Domain.Interface;
+using Gmom.Domain.Models;
+
+namespace Gmom.Infrastructure.Stores;
+
+public class PostgresConnectionStore : IPostgresConnectionStore
+{
+    public PostgresConnectionModel Value { get; set; } =
+        new()
+        {
+            Host = string.Empty,
+            Port = string.Empty,
+            Database = string.Empty,
+            Username = string.Empty,
+            Password = string.Empty,
+        };
+}
