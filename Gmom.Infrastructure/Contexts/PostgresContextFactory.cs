bbd2d70265a22868ee5ae361d7e4643dcecbd017
jotave <skai7ami@gmail.com>
2025-02-20 07:40:09 -0300

    create postgres context

---
+++ Gmom.Infrastructure/Contexts/PostgresContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Gmom.Infrastructure.Stores;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace Gmom.Infrastructure.Contexts;
+
+public class PostgresContextFactory(PostgresConnectionStore connectionStore)
+    : IDbContextFactory<PostgresContext>
+{
+    public PostgresContext CreateDbContext()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<PostgresContext>();
+        var pgBuilder = new NpgsqlConnectionStringBuilder
+        {
+            Host = connectionStore.Value.Host,
+            Port = int.Parse(connectionStore.Value.Port),
+            Database = connectionStore.Value.Database,
+            Username = connectionStore.Value.Username,
+            Password = connectionStore.Value.Password,
+        };
+        optionsBuilder.UseNpgsql(pgBuilder.ToString(), pg => pg.SetPostgresVersion(9, 5));
+        return new PostgresContext(optionsBuilder.Options);
+    }
+}
---
+++ Gmom.Infrastructure/Contexts/PostgresDesignTimeContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Gmom.Infrastructure.Contexts;
+
+public class PostgresDesignTimeContextFactory : IDesignTimeDbContextFactory<PostgresContext>
+{
+    public PostgresContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<PostgresContext>();
+        optionsBuilder.UseNpgsql(string.Empty, pg => pg.SetPostgresVersion(9, 5));
+        return new PostgresContext(optionsBuilder.Options);
+    }
+}
