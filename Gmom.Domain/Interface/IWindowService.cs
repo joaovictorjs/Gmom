1e2b5bc9231acff9162798ec4a6c9167e7c44331
jotave <skai7ami@gmail.com>
2025-02-21 13:09:29 -0300

    create window service

---
+++ Gmom.Domain/Interface/IWindowService.cs
@@ -0,0 +1,10 @@
+﻿namespace Gmom.Domain.Interface;
+
+public interface IWindowService<TWindow, TViewModel>
+    where TWindow : class
+    where TViewModel : class
+{
+    IWindowService<TWindow, TViewModel> Create(params KeyValuePair<string, object?>[] namedParameters);
+
+    bool? ShowDialog();
+}
---
+++ Gmom.Presentation/Services/WindowService.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using Autofac;
+using Gmom.Domain.Interface;
+
+namespace Gmom.Presentation.Services;
+
+public class WindowService<TWindow, TViewModel>(IComponentContext componentContext)
+    : IWindowService<TWindow, TViewModel>
+    where TWindow : Window
+    where TViewModel : class
+{
+    private TWindow? _window;
+    private TViewModel? _viewModel;
+
+    public IWindowService<TWindow, TViewModel> Create(
+        params KeyValuePair<string, object?>[] namedParameters
+    )
+    {
+        _window = componentContext.Resolve<TWindow>();
+        var parameters = namedParameters
+            .ToList()
+            .Select(pair => new NamedParameter(pair.Key, pair.Value));
+        _viewModel = componentContext.Resolve<TViewModel>(parameters);
+        _window.DataContext = _viewModel;
+        return this;
+    }
+
+    public bool? ShowDialog()
+    {
+        return _window?.ShowDialog();
+    }
+}
