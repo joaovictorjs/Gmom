be4bda9229d05eb60389e5c74dba383a19859d82
jotave <skai7ami@gmail.com>
2025-02-21 20:47:30 -0300

    add new migrations

---
+++ Gmom.Infrastructure/Migrations/20250221205709_NomeUnico.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gmom.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NomeUnico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_usuarios_nome",
+                table: "usuarios",
+                column: "nome",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_usuarios_nome",
+                table: "usuarios");
+        }
+    }
+}
