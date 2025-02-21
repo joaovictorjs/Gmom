da53dba6cc12e40507c08f89c34f63aabbe564db
jotave <skai7ami@gmail.com>
2025-02-21 14:00:35 -0300

    create user model

---
+++ Gmom.Domain/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using Gmom.Domain.Entities;
+using Gmom.Domain.Interface;
+
+namespace Gmom.Domain.Models;
+
+public class UserModel : IModel
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Password { get; set; }
+    public required bool IsAdmin { get; set; }
+
+    public IEntity ToEntity()
+    {
+        return new UserEntity
+        {
+            Id = Id,
+            Name = Name,
+            Password = Password,
+            IsAdmin = IsAdmin,
+        };
+    }
+}
