908e5eaadc5ca23d417a2ef8f5d6866efb7e3482
jotave <skai7ami@gmail.com>
2025-02-22 10:00:25 -0300

    operations to fetch and write users

---
+++ Gmom.Domain/Interface/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Gmom.Domain.Models;
+
+namespace Gmom.Domain.Interface;
+
+public interface IUserService
+{
+    Task<bool> HasAdmin();
+    int GetNextId();
+    Task<bool> Update(UserModel user, bool insert);
+    Task<bool> Delete(UserModel user);
+}
---
+++ Gmom.Infrastructure/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Gmom.Domain.Constants;
+using Gmom.Domain.Entities;
+using Gmom.Domain.Interface;
+using Gmom.Domain.Models;
+
+namespace Gmom.Infrastructure.Services;
+
+public class UserService(IRepository<UserEntity> repository) : IUserService
+{
+    public async Task<bool> HasAdmin()
+    {
+        return (await repository.WhereAsync(it => it.IsAdmin)).Any();
+    }
+
+    public int GetNextId()
+    {
+        return repository.NextValueForSequence(SequenceNames.Users);
+    }
+
+    public async Task<bool> Update(UserModel user, bool insert)
+    {
+        user.Password = toMD5(user.Password);
+
+        if (insert)
+        {
+            return await repository.InsertAsync((UserEntity)user.ToEntity()) > 0;
+        }
+
+        return await repository.UpdateAsync((UserEntity)user.ToEntity()) > 0;
+    }
+
+    private string toMD5(string content)
+    {
+        var encoded = Encoding.UTF8.GetBytes(content);
+        var hash = MD5.HashData(encoded);
+        return BitConverter.ToString(hash).Replace("-", string.Empty).ToLower();
+    }
+
+    public async Task<bool> Delete(UserModel user)
+    {
+        return await repository.DeleteAsync((UserEntity)user.ToEntity()) > 0;
+    }
+}
