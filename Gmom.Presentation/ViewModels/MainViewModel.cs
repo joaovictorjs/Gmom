60590a30b54590ab81e3386fe3364ef3105d6a77
jotave <skai7ami@gmail.com>
2025-02-23 10:46:34 -0300

    add status bar and F12 hotkey

--- Gmom.Presentation/ViewModels/MainViewModel.cs
+++ Gmom.Presentation/ViewModels/MainViewModel.cs
@@ -1,5 +1,55 @@
-﻿namespace Gmom.Presentation.ViewModels;
+﻿using System.Windows;
+using Gmom.Domain.Interface;
+using Gmom.Domain.Models;
+using Gmom.Infrastructure.Stores;
+using Gmom.Presentation.Views;
 
-public class MainViewModel : BindableBase
+namespace Gmom.Presentation.ViewModels;
+
+public class MainViewModel : BindableBase, IClosableWindow
 {
+    private readonly ICurrentUserStore _currentUserStore;
+    private IWindowService<LoginView, LoginViewModel> _loginViewService;
+
+    public string Host { get; }
+    public string Port { get; }
+    public string Database { get; }
+    public string Name { get; }
+    public bool IsAdmin { get; }
+
+    public DelegateCommand LogoutCommand { get; }
+
+    public MainViewModel(
+        ICurrentUserStore currentUserStore,
+        IPostgresConnectionStore connectionStore,
+        IWindowService<LoginView, LoginViewModel> loginViewService
+    )
+    {
+        _currentUserStore = currentUserStore;
+        _loginViewService = loginViewService;
+
+        Host = connectionStore.Value.Host;
+        Port = connectionStore.Value.Port;
+        Database = connectionStore.Value.Database;
+        Name = currentUserStore.Value.Name;
+        IsAdmin = currentUserStore.Value.IsAdmin;
+
+        LogoutCommand = new DelegateCommand(Logout);
+    }
+
+    private void Logout()
+    {
+        _currentUserStore.Value = new UserModel
+        {
+            Id = 0,
+            Name = string.Empty,
+            Password = string.Empty,
+            IsAdmin = false,
+        };
+
+        _loginViewService.Create().Show();
+        Close?.Invoke();
+    }
+
+    public Action? Close { get; set; }
 }
--- Gmom.Presentation/Views/MainView.xaml.cs
+++ Gmom.Presentation/Views/MainView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using MahApps.Metro.Controls;
+﻿using Gmom.Domain.Interface;
+using Gmom.Presentation.ViewModels;
+using MahApps.Metro.Controls;
 
 namespace Gmom.Presentation.Views;
 
@@ -7,5 +9,15 @@ public partial class MainView : MetroWindow
     public MainView()
     {
         InitializeComponent();
+    }
+
+    protected override void OnInitialized(EventArgs e)
+    {
+        base.OnInitialized(e);
+
+        if (DataContext is IClosableWindow closableWindow)
+        {
+            closableWindow.Close = Close;
+        }
     }
 }
