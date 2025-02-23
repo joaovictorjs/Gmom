fe59f21ef32a129ce211d490b4833d5631f47578
jotave <skai7ami@gmail.com>
2025-02-22 22:54:17 -0300

    implementing save method

--- Gmom.Presentation/ViewModels/SetupConnectionViewModel.cs
+++ Gmom.Presentation/ViewModels/SetupConnectionViewModel.cs
@@ -1,9 +1,14 @@
 ﻿using System.Windows;
+using Gmom.Domain.Interface;
+using Gmom.Infrastructure.Exceptions;
 
 namespace Gmom.Presentation.ViewModels;
 
 public class SetupConnectionViewModel : BindableBase
 {
+    private readonly IMigrationService _migrationService;
+    private readonly IMessageBoxService _messageBoxService;
+
     private string _host = string.Empty;
     private string _port = string.Empty;
     private string _database = string.Empty;
@@ -44,24 +49,37 @@ public class SetupConnectionViewModel : BindableBase
 
     public AsyncDelegateCommand SaveCommand { get; }
 
-    public SetupConnectionViewModel()
+    public SetupConnectionViewModel(
+        IMessageBoxService messageBoxService,
+        IMigrationService migrationService
+    )
     {
+        _messageBoxService = messageBoxService;
+        _migrationService = migrationService;
+        
         SaveCommand = new AsyncDelegateCommand(Save, CanSave);
     }
 
-
     private bool CanSave()
     {
-         return !string.IsNullOrWhiteSpace(Host)
-                      && !string.IsNullOrWhiteSpace(Port)
-                      && !string.IsNullOrWhiteSpace(Database)
-                      && !string.IsNullOrWhiteSpace(Username)
-                      && !string.IsNullOrWhiteSpace(Password) && !IsLoading;
+        return !string.IsNullOrWhiteSpace(Host)
+            && !string.IsNullOrWhiteSpace(Port)
+            && !string.IsNullOrWhiteSpace(Database)
+            && !string.IsNullOrWhiteSpace(Username)
+            && !string.IsNullOrWhiteSpace(Password)
+            && !IsLoading;
     }
 
-    private Task Save()
+    private async Task Save()
     {
-        throw new NotImplementedException();
+        try
+        {
+            await _migrationService.MigrateAsync();
+        }
+        catch (Exception ex)
+        {
+            ex.UseGlobalHandle(_messageBoxService.ShowError);
+        }
     }
 
     protected override bool SetProperty<T>(ref T storage, T value, string? propertyName = null)
