1c44fa17f8a6f60da8513d2d16e964bd69bbba7f
jotave <skai7ami@gmail.com>
2025-02-23 01:19:27 -0300

    handle InvalidOperationException

--- Gmom.Infrastructure/Exceptions/GlobalExceptionHandler.cs
+++ Gmom.Infrastructure/Exceptions/GlobalExceptionHandler.cs
@@ -2,12 +2,15 @@
 
 namespace Gmom.Infrastructure.Exceptions;
 
-public abstract class GlobalExceptionHandler
+public abstract partial class GlobalExceptionHandler
 {
     public static void Handle(Exception exception, Action<string> action)
     {
         switch (exception)
         {
+            case InvalidOperationException invalidOperationException:
+                HandleInvalidOperationException(invalidOperationException, action);
+                break;
             default:
                 ExceptionDispatchInfo.Capture(exception).Throw();
                 break;
