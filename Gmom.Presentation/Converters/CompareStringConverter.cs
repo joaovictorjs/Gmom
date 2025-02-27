2b7cb28a7590ceedbe7d8bc7f2d42fabc47ef3a8
jotave <skai7ami@gmail.com>
2025-02-27 17:41:31 -0300

    add default styles

---
+++ Gmom.Presentation/Converters/CompareStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Gmom.Presentation.Converters;
+
+public class CompareStringConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string stringValue || parameter is not string stringParameter)
+        {
+            return false;
+        }
+
+        return stringValue == stringParameter;
+    }
+
+    public object? ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    )
+    {
+        if (value is not string stringValue || parameter is not string stringParameter)
+        {
+            return false;
+        }
+        
+        return stringValue != stringParameter;
+    }
+}
