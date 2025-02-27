e07696e006f8d5ce5376a865ab8933567f25c944
jotave <skai7ami@gmail.com>
2025-02-27 17:44:05 -0300

    create PercentageToDimensionConverter

---
+++ Gmom.Presentation/Converters/PercentageToDimensionConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Gmom.Presentation.Converters;
+
+public class PercentageToDimensionConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        ArgumentNullException.ThrowIfNull(parameter);
+
+        return double.Parse(parameter.ToString()!) / 100 * double.Parse(value.ToString()!);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
