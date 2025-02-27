afd4ca97534242179f74e047a3e9821aed6cff18
jotave <skai7ami@gmail.com>
2025-02-27 17:42:54 -0300

    create TilesViewModel

--- Gmom.Presentation/DependencyInjection/RegisterViewModels.cs
+++ Gmom.Presentation/DependencyInjection/RegisterViewModels.cs
@@ -14,5 +14,7 @@ public partial class Injector
         Builder.RegisterType<MainViewModel>();
         
         Builder.RegisterType<SessionViewModel>();
+        
+        Builder.RegisterType<TilesViewModel>();
     }
 }
---
+++ Gmom.Presentation/ViewModels/TilesViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Gmom.Presentation.Events;
+using MahApps.Metro.Controls;
+
+namespace Gmom.Presentation.ViewModels;
+
+public class TilesViewModel : BindableBase
+{
+    private IEventAggregator _eventAggregator;
+    
+    public DelegateCommand<string> OpenFlyoutCommand { get; }
+    
+    public TilesViewModel(IEventAggregator eventAggregator)
+    {
+        _eventAggregator = eventAggregator;
+        
+        OpenFlyoutCommand = new DelegateCommand<string>(OpenFlyout);
+    }
+
+    private void OpenFlyout(string flyoutName)
+    {
+        _eventAggregator.GetEvent<FlyoutOpened>().Publish(flyoutName);
+    }
+}
