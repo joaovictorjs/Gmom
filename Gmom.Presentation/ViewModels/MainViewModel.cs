7619ccfa6be67e517d6d590b933a22cb05b8ab56
jotave <skai7ami@gmail.com>
2025-02-28 07:36:48 -0300

    subscribe to FlyoutOpened event

--- Gmom.Presentation/ViewModels/MainViewModel.cs
+++ Gmom.Presentation/ViewModels/MainViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 using Gmom.Domain.Interface;
 using Gmom.Domain.Models;
 using Gmom.Infrastructure.Stores;
+using Gmom.Presentation.Events;
 using Gmom.Presentation.Views;
+using MahApps.Metro.Controls;
 
 namespace Gmom.Presentation.ViewModels;
 
@@ -11,17 +14,33 @@ public class MainViewModel : BindableBase, IClosableWindow
     private readonly ICurrentUserStore _currentUserStore;
     private readonly IWindowService<LoginView, LoginViewModel> _loginViewService;
 
+    private string _currentFlyout = string.Empty;
+
+    public string CurrentFlyout
+    {
+        get => _currentFlyout;
+        set => SetProperty(ref _currentFlyout, value);
+    }
+
     public DelegateCommand LogoutCommand { get; }
 
     public MainViewModel(
         ICurrentUserStore currentUserStore,
-        IWindowService<LoginView, LoginViewModel> loginViewService
+        IWindowService<LoginView, LoginViewModel> loginViewService,
+        IEventAggregator eventAggregator
     )
     {
         _currentUserStore = currentUserStore;
         _loginViewService = loginViewService;
 
         LogoutCommand = new DelegateCommand(Logout);
+
+        eventAggregator.GetEvent<FlyoutOpened>().Subscribe(FlyoutOpened);
+    }
+
+    private void FlyoutOpened(string flyoutName)
+    {
+        CurrentFlyout = CurrentFlyout == flyoutName ? string.Empty : flyoutName;
     }
 
     private void Logout()
