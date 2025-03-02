00a613726c6ad6035e5b0337169e3a9efeadf32f
jotave <skai7ami@gmail.com>
2025-03-02 17:35:52 -0300

    make it MetroWindow

--- Gmom.Presentation/Views/ProductUpdateView.xaml.cs
+++ Gmom.Presentation/Views/ProductUpdateView.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System.Windows;
+using MahApps.Metro.Controls;
 
 namespace Gmom.Presentation.Views;
 
-public partial class ProductUpdateView : Window
+public partial class ProductUpdateView : MetroWindow
 {
     public ProductUpdateView()
     {
