afb212cddbe0aa47635fa990040e6691a33590da
jotave <skai7ami@gmail.com>
2025-03-06 21:15:42 -0300

    add GenerateBarCode and Save methods

--- Gmom.Domain/Interface/IProductService.cs
+++ Gmom.Domain/Interface/IProductService.cs
@@ -1,6 +1,10 @@
-﻿namespace Gmom.Domain.Interface;
+﻿using Gmom.Domain.Models;
+
+namespace Gmom.Domain.Interface;
 
 public interface IProductService
 {
     int GetNextId();
+    string GenerateBarCode();
+    Task Save(ProductModel product, bool isUpdate);
 }
--- Gmom.Infrastructure/Services/ProductService.cs
+++ Gmom.Infrastructure/Services/ProductService.cs
@@ -1,13 +1,57 @@
 ﻿using Gmom.Domain.Constants;
 using Gmom.Domain.Entities;
+using Gmom.Domain.Exceptions;
 using Gmom.Domain.Interface;
+using Gmom.Domain.Models;
 
 namespace Gmom.Infrastructure.Services;
 
-public class ProductService(IRepository<ProductEntity> repository) : IProductService
+public class ProductService(
+    IRepository<ProductEntity> repository,
+    ICurrentUserService currentUserService
+) : IProductService
 {
     public int GetNextId()
     {
         return repository.NextValueForSequence(SequenceNames.Products);
+    }
+
+    public string GenerateBarCode()
+    {
+        var result = string.Empty;
+        var random = new Random();
+        for (var c = 0; c < 13; c++)
+            result += random.Next(9);
+        return result;
+    }
+
+    public async Task Save(ProductModel product, bool isUpdate)
+    {
+        await currentUserService.CheckIsAdmin();
+
+        await CheckBarCode(product.BarCode, product.Id);
+
+        if (isUpdate)
+        {
+            await repository.UpdateAsync((ProductEntity)product.ToEntity());
+        }
+        else
+        {
+            await repository.InsertAsync((ProductEntity)product.ToEntity());
+        }
+    }
+
+    private async Task CheckBarCode(string productBarCode, int productId)
+    {
+        var barCodeCheck = await repository.WhereAsync(it =>
+            it.BarCode == productBarCode && it.Id != productId
+        );
+
+        if (barCodeCheck.Any())
+        {
+            throw new DuplicatedValueException(
+                $"O código de barras {productBarCode} ja está sendo utilizado!"
+            );
+        }
     }
 }
