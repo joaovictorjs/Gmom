2bcb0803c6b60d5240f0c2603f8a2a52cdd0f286
jotave <skai7ami@gmail.com>
2025-03-14 07:32:20 -0300

    create Boolean converters

---
+++ Gmom.Presentation/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Gmom.Presentation.Converters;
+
+public class InverseBooleanConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not bool boolValue)
+        {
+            throw new ArgumentException("Value must be of type bool");
+        }
+        
+        return !boolValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Gmom.Presentation/Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Gmom.Presentation.Converters;
+
+public class InverseBooleanToVisibilityConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not bool boolValue)
+        {
+            throw new ArgumentException("Value must be of type bool");
+        }
+        
+        return boolValue ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
