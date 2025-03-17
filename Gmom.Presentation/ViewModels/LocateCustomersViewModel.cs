ff7ae4f90f86993024f38df47487554aaaf3ccd1
jotave <skai7ami@gmail.com>
2025-03-17 13:39:35 -0300

    add insert command

--- Gmom.Presentation/ViewModels/LocateCustomersViewModel.cs
+++ Gmom.Presentation/ViewModels/LocateCustomersViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace Gmom.Presentation.ViewModels;
+﻿using Gmom.Domain.Interface;
+using Gmom.Presentation.Views;
+
+namespace Gmom.Presentation.ViewModels;
 
 public class LocateCustomersViewModel : BindableBase
 {
-    
+    private readonly IWindowService<
+        InsertOrUpdateCustomerView,
+        InsertOrUpdateCustomerViewModel
+    > _customerWindowService;
+
+    public DelegateCommand InsertCustomerCommand { get; }
+
+    public LocateCustomersViewModel(
+        IWindowService<
+            InsertOrUpdateCustomerView,
+            InsertOrUpdateCustomerViewModel
+        > customerWindowService
+    )
+    {
+        _customerWindowService = customerWindowService;
+
+        InsertCustomerCommand = new DelegateCommand(InsertCustomer);
+    }
+
+    private void InsertCustomer()
+    {
+        _customerWindowService.Create().ShowDialog();
+    }
 }
