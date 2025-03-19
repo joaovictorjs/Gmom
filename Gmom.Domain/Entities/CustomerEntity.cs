bced365c7944cadaba636ad097c6e7ae51f35b96
jotave <skai7ami@gmail.com>
2025-03-19 07:25:59 -0300

    rename "clientes" table columns

--- Gmom.Domain/Entities/CustomerEntity.cs
+++ Gmom.Domain/Entities/CustomerEntity.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Gmom.Domain.Interface;
 using Gmom.Domain.Models;
 
@@ -7,7 +8,10 @@ namespace Gmom.Domain.Entities;
 [Table("clientes")]
 public class CustomerEntity:IEntity
 {
+    [Column("id"), Key]
     public required int Id {get;set;}
+    
+    [Column("nome")]
     public required string Name {get;set;}
     
     public IModel ToModel()
---
+++ Gmom.Infrastructure/Migrations/20250318203129_RenomearColunasDaTabelaClientes.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gmom.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenomearColunasDaTabelaClientes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "clientes",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "clientes",
+                newName: "nome");
+            
+            migrationBuilder.RenameIndex("clientes_Id_seq", "clientes_id_seq");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "clientes",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "nome",
+                table: "clientes",
+                newName: "Name");
+            
+            migrationBuilder.RenameIndex("clientes_id_seq" , "clientes_Id_seq");
+        }
+    }
+}
