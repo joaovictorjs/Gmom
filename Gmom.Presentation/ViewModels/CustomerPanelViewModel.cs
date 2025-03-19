f5ad0f982291b270dde8f01a8d33b3402a5df4c4
jotave <skai7ami@gmail.com>
2025-03-19 17:46:15 -0300

    create customer panel viewmodel

---
+++ Gmom.Presentation/ViewModels/CustomerPanelViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Gmom.Presentation.Events;
+
+namespace Gmom.Presentation.ViewModels;
+
+public class CustomerPanelViewModel : BindableBase
+{
+    private readonly IEventAggregator _eventAggregator;
+    private string _customerName = string.Empty;
+
+    public string CustomerName
+    {
+        get => _customerName;
+        set => SetProperty(ref _customerName, value);
+    }
+
+    public CustomerPanelViewModel(IEventAggregator eventAggregator)
+    {
+        _eventAggregator = eventAggregator;
+
+        _eventAggregator
+            .GetEvent<CustomerSelected>()
+            .Subscribe(customer => CustomerName = customer.Name);
+    }
+}
