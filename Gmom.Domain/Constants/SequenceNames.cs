577567d538df1ff5fab02de416c69e31fe248e1d
jotave <skai7ami@gmail.com>
2025-03-19 07:28:16 -0300

    add Customers

--- Gmom.Domain/Constants/SequenceNames.cs
+++ Gmom.Domain/Constants/SequenceNames.cs
@@ -4,4 +4,5 @@ public static class SequenceNames
 {
     public const string Users = "usuarios_id_seq";
     public const string Products = "produtos_id_seq";
+    public const string Customers = "clientes_id_seq";
 }
