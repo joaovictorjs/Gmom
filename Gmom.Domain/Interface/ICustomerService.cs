44a9e56f6f2518dda4d5fa461dd75f3163c19a31
jotave <skai7ami@gmail.com>
2025-03-19 07:40:35 -0300

    implement Save

--- Gmom.Domain/Interface/ICustomerService.cs
+++ Gmom.Domain/Interface/ICustomerService.cs
@@ -1,6 +1,9 @@
-﻿namespace Gmom.Domain.Interface;
+﻿using Gmom.Domain.Models;
+
+namespace Gmom.Domain.Interface;
 
 public interface ICustomerService
 {
     int GetNextId();
+    Task Save(CustomerModel customer, bool isUpdate);
 }
--- Gmom.Infrastructure/Services/CustomerService.cs
+++ Gmom.Infrastructure/Services/CustomerService.cs
@@ -1,13 +1,31 @@
 ﻿using Gmom.Domain.Constants;
 using Gmom.Domain.Entities;
 using Gmom.Domain.Interface;
+using Gmom.Domain.Models;
 
 namespace Gmom.Infrastructure.Services;
 
-public class CustomerService(IRepository<CustomerEntity> repository, ICurrentUserService currentUserService):ICustomerService
+public class CustomerService(
+    IRepository<CustomerEntity> repository,
+    ICurrentUserService currentUserService
+) : ICustomerService
 {
     public int GetNextId()
     {
         return repository.NextValueForSequence(SequenceNames.Customers);
+    }
+
+    public async Task Save(CustomerModel customer, bool isUpdate)
+    {
+        await currentUserService.CheckIsAdmin();
+
+        if (isUpdate)
+        {
+            await repository.UpdateAsync((CustomerEntity)customer.ToEntity());
+        }
+        else
+        {
+            await repository.InsertAsync((CustomerEntity)customer.ToEntity());
+        }
     }
 }
