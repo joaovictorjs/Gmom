f4888ed1e2acec19d435a6b3d450b00768df3158
jotave <skai7ami@gmail.com>
2025-03-19 07:46:46 -0300

    implement ChangeFindStrategyCommand

--- Gmom.Presentation/ViewModels/LocateCustomersViewModel.cs
+++ Gmom.Presentation/ViewModels/LocateCustomersViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Gmom.Domain.Interface;
+﻿using Gmom.Domain.Enums;
+using Gmom.Domain.Interface;
+using Gmom.Presentation.Events;
 using Gmom.Presentation.Views;
 
 namespace Gmom.Presentation.ViewModels;
@@ -10,6 +12,15 @@ public class LocateCustomersViewModel : BindableBase
         InsertOrUpdateCustomerViewModel
     > _customerWindowService;
 
+    private string _findStrategyName = "Name";
+
+    public string FindStrategyName
+    {
+        get => _findStrategyName;
+        set => SetProperty(ref _findStrategyName, value);
+    }
+
+    public DelegateCommand<string> ChangeFindStrategyCommand { get; }
     public DelegateCommand InsertCustomerCommand { get; }
 
     public LocateCustomersViewModel(
@@ -21,7 +32,13 @@ public class LocateCustomersViewModel : BindableBase
     {
         _customerWindowService = customerWindowService;
 
+        ChangeFindStrategyCommand = new DelegateCommand<string>(ChangeFindStrategy);
         InsertCustomerCommand = new DelegateCommand(InsertCustomer);
+    }
+
+    private void ChangeFindStrategy(string strategy)
+    {
+        FindStrategyName = strategy;
     }
 
     private void InsertCustomer()
