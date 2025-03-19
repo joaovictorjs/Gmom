b49956f41378e36d4ce8845cb1428021e238691b
jotave <skai7ami@gmail.com>
2025-03-19 07:28:08 -0300

    implement GetNextId

---
+++ Gmom.Domain/Interface/ICustomerService.cs
@@ -0,0 +1,6 @@
+﻿namespace Gmom.Domain.Interface;
+
+public interface ICustomerService
+{
+    int GetNextId();
+}
---
+++ Gmom.Infrastructure/Services/CustomerService.cs
@@ -0,0 +1,13 @@
+﻿using Gmom.Domain.Constants;
+using Gmom.Domain.Entities;
+using Gmom.Domain.Interface;
+
+namespace Gmom.Infrastructure.Services;
+
+public class CustomerService(IRepository<CustomerEntity> repository, ICurrentUserService currentUserService):ICustomerService
+{
+    public int GetNextId()
+    {
+        return repository.NextValueForSequence(SequenceNames.Customers);
+    }
+}
