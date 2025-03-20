d6df3aec74fcbc5fb9dbe677962f9de1c791a10b
jotave <skai7ami@gmail.com>
2025-03-20 20:42:36 -0300

    implement ToBrazilianReal

---
+++ Gmom.Domain/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Gmom.Domain.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToBrazilianReal(this string str, string fallback)
+    {
+        if (string.IsNullOrWhiteSpace(str) || str.Trim().Equals("R$"))
+        {
+            return string.Empty;
+        }
+
+        var number = str.Replace("R$", string.Empty).Trim();
+
+        return !double.TryParse(number, NumberStyles.Currency, CultureInfo.CurrentCulture, out _)
+            ? fallback
+            : $"R$ {number}";
+    }
+}
