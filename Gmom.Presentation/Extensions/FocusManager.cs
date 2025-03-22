658ef4be895b54b0e528a184ca852fb39a525848
jotave <skai7ami@gmail.com>
2025-03-22 11:41:08 -0300

    create FocusManager

---
+++ Gmom.Presentation/Extensions/FocusManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
+namespace Gmom.Presentation.Extensions;
+
+public static class FocusManager
+{
+    private static readonly DependencyProperty PressEnterToMoveFocusProperty =
+        DependencyProperty.RegisterAttached(
+            "PressEnterToMoveFocus",
+            typeof(bool),
+            typeof(FocusManager),
+            new UIPropertyMetadata(OnPressEnterMoveFocusChanged)
+        );
+
+    public static bool GetPressEnterToMoveFocus(DependencyObject target)
+    {
+        return (bool)target.GetValue(PressEnterToMoveFocusProperty);
+    }
+
+    public static void SetPressEnterToMoveFocus(DependencyObject target, bool value)
+    {
+        target.SetValue(PressEnterToMoveFocusProperty, value);
+    }
+
+    private static void OnPressEnterMoveFocusChanged(
+        DependencyObject d,
+        DependencyPropertyChangedEventArgs e
+    )
+    {
+        if (d is not UIElement element)
+            return;
+
+        if ((bool)e.NewValue)
+        {
+            element.KeyDown += OnKeyDown;
+        }
+        else
+        {
+            element.KeyDown -= OnKeyDown;
+        }
+    }
+
+    private static void OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter)
+            return;
+        var element = sender as UIElement;
+        element?.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+    }
+
+    private static readonly DependencyProperty FocusProperty =
+        DependencyProperty.RegisterAttached(
+            "Focus",
+            typeof(bool),
+            typeof(FocusManager),
+            new UIPropertyMetadata(OnInitialFocusChanged)
+        );
+
+    public static bool GetFocus(DependencyObject target)
+    {
+        return (bool)target.GetValue(FocusProperty);
+    }
+
+    public static void SetFocus(DependencyObject target, bool value)
+    {
+        target.SetValue(FocusProperty, value);
+    }
+
+    private static void OnInitialFocusChanged(
+        DependencyObject d,
+        DependencyPropertyChangedEventArgs e
+    )
+    {
+        if (d is not FrameworkElement element)
+            return;
+
+        if (!(bool)e.NewValue)
+            return;
+
+        element.Loaded += OnLoaded;
+    }
+
+    private static void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is not FrameworkElement element) return;
+        
+        element.Focus();
+        element.Loaded -= OnLoaded;
+    }
+}
