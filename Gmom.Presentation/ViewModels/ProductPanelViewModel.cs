4061a64ebdeb189dbb18260919e9948e8d5993ea
jotave <skai7ami@gmail.com>
2025-03-26 07:56:24 -0300

    create SplitQuantityAndProduct and RemoveInvalidChars

--- Gmom.Presentation/ViewModels/ProductPanelViewModel.cs
+++ Gmom.Presentation/ViewModels/ProductPanelViewModel.cs
@@ -1,10 +1,46 @@
 ﻿using System.Globalization;
+using System.Runtime.InteropServices.JavaScript;
 using Gmom.Domain.Extensions;
 
 namespace Gmom.Presentation.ViewModels;
 
 public class ProductPanelViewModel : BindableBase
 {
+    private double _quantity = 1.0;
+    private string _idBarCode = string.Empty;
+
+    private string _quantityVersusProduct = string.Empty;
+    public string QuantityVersusProduct
+    {
+        get => _quantityVersusProduct;
+        set
+        {
+            SetProperty(ref _quantityVersusProduct, RemoveInvalidChars(value));
+
+            SplitQuantityAndProduct(value);
+        }
+    }
+
+    private static string RemoveInvalidChars(string value)
+    {
+        var chars = value
+            .Where(it => char.IsNumber(it) || it == '*' || char.IsWhiteSpace(it))
+            .ToArray();
+
+        return new string(chars);
+    }
+
+    private void SplitQuantityAndProduct(string value)
+    {
+        if (value.IsNullOrWhiteSpace())
+            return;
+
+        var values = value.Split('*', 2).Select(it => it.Trim()).ToList();
+
+        _quantity = values.Count == 1 ? 1 : double.Parse(values[0], CultureInfo.CurrentCulture);
+        _idBarCode = values.Count == 1 ? values[0] : values[1];
+    }
+
     private string _price = string.Empty;
     public string Price
     {
