8563fcc99c1922a881c60b77bd0a11dd726b5608
jotave <skai7ami@gmail.com>
2025-03-31 08:35:08 -0300

    create CartModel

---
+++ Gmom.Domain/Models/CartModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Gmom.Domain.Models;
+
+public class CartModel
+{ 
+    public required string Name { get; set; }
+    public required string BarCode { get; set; }
+    public required double Price { get; set; }
+    public required double Quantity { get; set; }
+}
