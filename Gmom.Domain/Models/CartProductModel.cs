846a7966a2f9bace3ad224a3f78ed1bfff097c1d
jotave <skai7ami@gmail.com>
2025-03-31 17:27:42 -0300

    add Id and Total

--- Gmom.Domain/Models/CartProductModel.cs
+++ Gmom.Domain/Models/CartProductModel.cs
@@ -4,9 +4,11 @@ namespace Gmom.Domain.Models;
 
 public class CartProductModel : IModel
 { 
+    public required int Id { get; set; }
     public required string Name { get; set; }
     public required string BarCode { get; set; }
     public required double Price { get; set; }
+    public required double Total { get; set; }
     public required double Quantity { get; set; }
     public IEntity ToEntity()
     {
